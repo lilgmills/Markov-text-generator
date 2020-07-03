264382225e9dcb7c9cd7dfb935e437e3fcb828e9
Loafaroo <53982793+Loafaroo@users.noreply.github.com>
2020-07-02 21:00:35 -0500

    Update poem.py

--- poem.py
+++ poem.py
@@ -1,45 +1,49 @@
-##opens a SampleText file, and creates pairs of words that occur, and then a dictionary of opening words mapped to their list of following words
+"""opens a SampleText file, and creates pairs of words that occur,
+and then a markov dictionary which is used to generate sample words with appropriate frequencies
+"""
 
 import os
-import numpy as np
+from random import choice
 
-def makepairs(sample):
-    for i in range(len(sample)-1):
-        yield (sample[i], sample[i+1])
+def not_empty(string):
+    return not string == ''
+
+def starter_pair(tup):
+    return not tup[0].islower()
+
+def listfromsample(sample_file):
+    """
+    puts a sample of text into a list of words (with punctuation) from start to finish
+    """
+    with open(sample_file, "r", encoding = "utf-8") as sample_text:
+        paragraphs = sample_text.read().split("\n")
         
-        ##the -2 range leaves off the \n entry, and leaves a
-        ##buffer position for creating pairs
+        ##creates paragraphs as a list of strings by splitting on line breaks
 
-def decode(string):
-    pass
-    ##returns a string of 
+    paragraph_iter = filter(not_empty, paragraphs)
 
-def main():
-    new_file = open("chain_words.txt", "w+")
-
-    with open("speeches.txt", "r", encoding = "utf-8") as file_text:
-        paragraphs = file_text.read().split("\n")
-        
-        ##creates paragraphs as a list of strings by splitting on spaces
-
-    paragraphs_lists = [para.split(" ") for para in paragraphs]
+    paragraphs_lists = [para.split(" ") for para in paragraph_iter]
     ##turns a list of paragraphs into a list of word lists
+    
     listwords = []
     for list in paragraphs_lists:
         listwords += list
 
         ##finishes putting paragraphs into list form
-        
 
-    #for i in range(len(listwords)):
-     #   print(listwords[i])
-            
-        
-    pairs = makepairs(listwords)
+    return listwords
 
+def makepairs(sample_list):
+    for i in range(len(sample_list)-1):
+        yield (sample_list[i], sample_list[i+1])
+
+def make3gram(list_words):
+    for i in range(len(list_words) -2):
+        yield (list_words[i], list_words[i+1], list_words[i+2])
+
+def markov_pairs_dict(iterable_pairs):
     word_dict = {}
-
-    for word1, word2 in pairs:
+    for word1, word2 in iterable_pairs:                
         if word1 in word_dict.keys():
             word_dict[word1].append(word2)
         else:
@@ -47,32 +51,53 @@ def main():
 
         ##puts each opener word in a dictionary if it isn't already there
 
-    for key in word_dict.keys():
-        try:
-            new_file.write(key + ':' +'[' + ', '.join(word_dict[key]) + ']' +'\n')
-        except:
-            if key:
-                print(word_dict[key])
-    ##create word dictionary string to go into a file
+    return word_dict
 
-    new_file.close()
+def markov_trigram_dict(iterable_trigrams):
+    tri_dict = {}
+    for word1, word2, word3 in iterable_trigrams:
+        if (word1, word2) in tri_dict.keys():
+            tri_dict[(word1, word2)].append(word3)
+        else:
+            tri_dict[(word1, word2)] = [word3]
 
-    first_word = np.random.choice(listwords)
-    while first_word.islower():
-        first_word = np.random.choice(listwords)
+    return tri_dict
+    
 
-    chain = [first_word]
-
-    num_words = 400
-
-    for i in range (num_words):
-        chain.append(np.random.choice(word_dict[chain[-1]]))
-    with open("newSpeechScript.txt", "w+") as f:
-        print(' '.join(chain) , file =f)
+def decode(string):
+    pass
+    """
+    if needed, a function to prepare encoding and formatting of individual words
+    """
 
 
-filepath = "C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python38-32\\Environment\\python poem"
-os.chdir(filepath)
+def main():
+    sample_file = "speeches.txt"         
+    listwords = listfromsample(sample_file)    
+    tris = make3gram(listwords)
+
+    tri_dict = markov_trigram_dict(tris)
+
+    num = 180
+
+    trikeylist = list(tri_dict.keys())
+    starter_pairs = list(filter(starter_pair, trikeylist))
+
+    chain = [choice(starter_pairs)]
+
+    for i in range(num):
+        selection = choice(tri_dict[chain[-1]])
+        chain.append((chain[-1][1], selection))
+
+    chain_words = [chain[0][0]]
+    for x, y in chain:
+        chain_words.append(y)
+
+    with open(f"NewScript{num}.txt", "w+") as f:
+        print(' '.join(chain_words), file =f)    
+    
+
+    
 
 if __name__ == "__main__":
     main()

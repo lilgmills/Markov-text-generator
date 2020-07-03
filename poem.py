a219be212ef90a6efd6b7168d279452eb57a043f
Loafaroo <53982793+Loafaroo@users.noreply.github.com>
2020-07-03 01:03:25 -0500

    Update poem.py
    Nicer usability for the frequency generator

--- poem.py
+++ poem.py
@@ -1,8 +1,8 @@
-"""opens a SampleText file, and creates pairs of words that occur,
+"""
+opens a SampleText file, and creates trigrams of words that occur,
 and then a markov dictionary which is used to generate sample words with appropriate frequencies
 """
 
-import os
 from random import choice
 
 def listfromsample(sample_file):
@@ -27,25 +27,9 @@ def listfromsample(sample_file):
 
     return listwords
 
-def makepairs(sample_list):
-    for i in range(len(sample_list)-1):
-        yield (sample_list[i], sample_list[i+1])
-
 def make3gram(list_words):
     for i in range(len(list_words) -2):
         yield (list_words[i], list_words[i+1], list_words[i+2])
-
-def markov_pairs_dict(iterable_pairs):
-    word_dict = {}
-    for word1, word2 in iterable_pairs:                
-        if word1 in word_dict.keys():
-            word_dict[word1].append(word2)
-        else:
-            word_dict[word1] = [word2]
-
-        ##puts each opener word in a dictionary if it isn't already there
-
-    return word_dict
 
 def markov_trigram_dict(iterable_trigrams):
     tri_dict = {}
@@ -88,10 +72,7 @@ def not_empty(string):
     return not string == ''
 
 def starter_pair(tup):
-    return not tup[0].islower() and len(tup[0]) > 1
-
-def quit(default = ""):
-    return default == "exit" or default == "quit"    
+    return not tup[0].islower() and len(tup[0]) > 1    
 
 def decode(string):
     pass
@@ -99,34 +80,50 @@ def decode(string):
     if needed, a function to prepare encoding and formatting of individual words
     """
 
-def Display(tri_dict):
-    new_default = input('\nPress Enter or type length of sample (default 40) (type "exit" to exit): ')    
-    try:
-        new_default = int(new_default)
-    except:
-        pass
-    if not quit(new_default):
-        if isinstance(new_default, int):
-            chain_words = create_chain_text(tri_dict, new_default)
-        else:
-            chain_words = create_chain_text(tri_dict)
-
-        print(' '.join(chain_words))
-
-    return new_default
+class DisplayChain():
     
+    def __init__(self, tri_dict, interpreter = True):
+        self.tri_dict = tri_dict
+        self.interpreter = interpreter
+        self.sessionID = 0
+        self.new_default = ""
 
-def main(default = True):
-    sample_file = "speeches.txt"         
+    def quit(self, default = ""):
+        return default == "exit" or default == "quit"
+        
+    def mainloop(self):
+        self.new_default = input('\nPress Enter or type length of sample (default 60) (type "exit" to exit): ')
+        if not self.quit(self.new_default):
+            try: self.new_default = int(self.new_default)
+            except: pass
+            
+            if isinstance(self.new_default, int):    
+                self.chain_words = create_chain_text(self.tri_dict, self.new_default)
+            else:
+                self.chain_words = create_chain_text(self.tri_dict)
+
+            if self.interpreter:
+                print(' '.join(self.chain_words))
+            else:
+                with open(f"newoutput{self.sessionID}.txt", "w+") as f:
+                    print(' '.join(self.chain_words), file =f)
+                print(f'outputted to newoutput{self.sessionID}.txt')
+                self.sessionID+=1
+
+    
+def main():
+    
+    sample_file = "speeches.txt"
+    
     listwords = listfromsample(sample_file)    
     tris = make3gram(listwords)
     tri_dict = markov_trigram_dict(tris)
-
-    while not quit(default):
-        default = Display(tri_dict)
-
     
-
+    f = DisplayChain(tri_dict)
+    while not f.quit(f.new_default):
+        f.mainloop()
+    
+    
 if __name__ == "__main__":
     main()
     
